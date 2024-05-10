ac7ff14c5982919e14a0345869f3fca24cad3bf4
Eve_acad <coc123steve@gmail.com>
2024-05-11 01:17:08 +1000

    first commit

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="p-6 2xl:px-40 container xl:mx-auto flex justify-between items-center min-w-full">
-    <img src="@/assets/logo.svg" />
+    <img src="@/assets/logo.png" style="width:100px;height:60px;" />
     <div class="hidden xl:flex text-sm items-center">
       <div class="flex gap-8">
-        <a href="https://dexscreener.com/"><div class="flex gap-1.5">Chart </div></a>
-        <a href="https://t.me/zestfestsol"><div class="flex gap-1.5">Telegram</div></a>
-        <a href="https://twitter.com/"><div class="flex gap-1.5">Twitter</div></a>
-        <a href="https://solscan.io/token/"><div class="flex gap-1.5">Solscan </div></a>
+        <a href="https://dexscreener.com/"><div class="flex gap-1.5">ZestyChart</div></a>
+        <a href="https://t.me/zestfestsol"><div class="flex gap-1.5">ZestyGram</div></a>
+        <a href="https://twitter.com/"><div class="flex gap-1.5">ZesTwitter</div></a>
+        <a href="https://solscan.io/token/"><div class="flex gap-1.5">ZestyScan</div></a>
 
       </div>
         <a href="https://www.pump.fun/board">
       <button
-        class="mx-5 px-4 text-xs gradient-blue text-white rounded-[4px] py-3 hover:border-[#3040E8] font-semibold border hover:text-[#1829d9]"
+        class="zest mx-5 px-4 text-xs gradient-blue text-white rounded-[4px] py-3 hover:border-[#3040E8] border hover:text-[#1829d9]" style="font-size: 30px"
       >
-        Book Now
+        I WANT TO COME TO ZEST FEST
       </button></a>
     </div>
     <Menu class="xl:hidden w-12 h-12 cursor-pointer" />
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,40 +2,31 @@
   <div class="gradient-dark-blue text-white">
     <div class="xl:container px-6 py-24 lg:py-40 mx-auto lg:grid grid-cols-10 gap-24">
       <div class="flex flex-col col-span-5 lg:col-span-4">
-        <div class="font-semibold text-lg">TONIGHT'S SPECIALS</div>
+        <div class="text-lg zest">ZEST FEST 2024</div>
         <div class="border-b-[1px] h-3 border-gray-500" />
-        <div class="mt-3 font-bold text-3xl leading-normal">ZEST FEST TALK SHOW</div>
-        <div class="bg-[#141c42] rounded-lg p-6 border mb-2 mt-8">
-          <div>🗓 Date: 8th May 2024</div>
-          <div>📍Venue: Pump.fun</div>
-          <div>⏰Time: 12.00 pm</div>
+        <div class="mt-3 font-bold text-3xl leading-normal">The most TALKED ABOUT zesty nigga linkup of the year!</div>
+        <div class="bg-[#c410c4] rounded-lg p-6 border mb-2 mt-8">
+          <div>Date: 8th May 2024</div>
+          <div>Venue: pump.fun</div>
+          <div>Time: Right now!</div>
         </div>
         <div class="text-justify mt-7">
-          Get ready to spice up your life at ZEST FEST! This isn't just any event; it's a
-          celebration of enthusiasm, energy, and the zestful spirit of the iconic Zesty LeBron and
-          Drake! Whether you're a fan of exhilarating talks, engaging discussions, or just   looking
-          to<span :class="show? '':'opacity-50'"> infuse a little zest into your routine, thisfest is where you need to be.</span>
+          ZEST FEST 2024 is getting gayer than ever! Get ready to oil up, twerk and edge at ZEST FEST! This isn't just any event; it's a
+          gathering of the zestiest niggas around! You've never seen shit as zesty as this!
+          Whether you're a fan of exhilarating twerk-offs, oiled-up naked massages with multiple black men, or just looking
+          to infuse a little zest into your life, ZESTFEST 2024 is where you need to be.
         </div>
-        <div class="mt-6 text-justify" :class="show? '':'opacity-30'">Why Attend ZEST FEST?</div>
+        <div class="mt-6 text-justify" style="font-size: 30px" :class="show? '':'opacity-70'"><strong>Why should you pull your zesty ass up to the ZEST FEST?</strong></div>
         <div v-if="show">
-          <div class="my-6">You will learn:</div>
           <div class="my-6">
-            • <strong>Inspiring Talks:</strong> Dive into a series of invigorating discussions led
-            by Zesty Drake himself. Explore topics that are as vibrant and dynamic as our speaker's
-            legendary zest for life.
+            <strong>OILED TWERK EDUCATION:</strong> Dive into a series of invigorating classes led
+            by BBL Drizzy himself. Once you learn what he has to teach, you'll never shake your gyatt the same again!
           </div>
           <div class="my-6">
-            • <strong>Opportunities:</strong> Mix and mingle with other zest enthusiasts. Who knows?
-            The connections you make here could lead to your next big adventure!
+            <strong>EDGING SEMINAR:</strong> Led by Ambatukam Ambassing, and attended by the leaders of the edging industry, this is a fucking game changer. Trust me bro. I went once and now I can't fucking stop edging.
           </div>
           <div class="my-6">
-            • <strong>Fun Activities:</strong> Engage in a variety of zest-themed activities
-            designed to energize and inspire. Leave with not just insights but also an unforgettable
-            experience.
-          </div>
-          <div class="my-6">
-            Tickets: Available now! Secure your spot for an unforgettable journey into the world of
-            zest.
+            <strong>DIRTY TALK MASTERCLASS:</strong> Is that dick good? YES KING! How good is that dick? *intense moaning* HOW GOOD IS THAT DICK BITCH? Amazing...
           </div>
         </div>
         <div class="mt-8 flex gap-3 items-center cursor-pointer mb-3" @click="show = !show">
@@ -46,53 +37,62 @@
       </div>
 
       <div class="col-span-5 lg:col-span-6 bg-white rounded-lg flex flex-col h-fit text-black">
-        <div class="mx-auto my-8 text-3xl font-medium">Register now</div>
-        <div class="grid grid-cols-2 gap-4 mx-auto">
+        <div class="mx-auto my-8 text-3xl font-medium">BUY SOME $ZEST NIGGA</div>
+        <div class="gap-4 mx-auto">
           <div class="flex flex-col gap-1.5">
-            <div>Business Email:*</div>
-            <InputText type="text" placeholder="Business Email*" />
-          </div>
-          <div class="flex flex-col gap-1.5">
-            <div>First Name:*</div>
-            <InputText type="text" placeholder="First Name*" />
-          </div>
-          <div class="flex flex-col gap-1.5">
-            <div>Last Name:*</div>
-            <InputText type="text" placeholder="Last Name*" />
-          </div>
-          <div class="flex flex-col gap-1.5">
-            <div>Company Name:*</div>
-            <InputText type="text" placeholder="Company Name*" />
-          </div>
-          <div class="flex flex-col gap-1.5">
-            <div>Job Title:*</div>
-            <InputText type="text" placeholder="Job Title*" />
-          </div>
-        </div>
+            <div>ZEST FEST 2023 WAS FUCKING CRAZY! 😭 YOU DON'T WANT TO MISS THIS 💯</div><br>
+            <video width="200" controls>
+  <source src="../assets/zest.mp4" type="video/mp4">
+</video>
+        </div></div>
 
         <button
           class="mx-auto text-sm px-4 my-8 w-fit gradient-blue-light text-white rounded-md py-3 hover:border-[#3040E8] font-semibold border"
         >
-          <a href="https://www.pump.fun/board"> SAVE MY SPOT</a>
+          <a href="https://www.pump.fun/board">FUCK YEAH I WANNA BUY SOME $ZEST</a>
         </button>
       </div>
     </div>
-    <div class="xl:container px-6 flex flex-col items-center gap-6 pb-24 min-w-max mx-auto">
-      <div class="font-bold text-2xl">Speakers</div>
+          <div class="font-bold text-2xl xl:container px-6 flex flex-col items-center gap-6 pb-24 min-w-max mx-auto" style="font-size: 50px">Hella niggas are coming btw</div>
+
+    <div class="xl:container px-6 gap-6 items-center min-w-max grid grid-cols-4 mx-auto">
       <img class="rounded-full w-40 h-40" src="@/assets/drake.jpg/" />
       <div>
-        <div class="text-center font-bold">Zesty Drake</div>
-        <div>President, Content Marketing and Beefer, ZEST FEST</div>
+        <div class="text-left font-bold">BBL Drizzy</div>
+        <div>Winner of "Zestiest Nigga" Award 2013-2023.</div>
       </div>
       <img class="rounded-full w-40 h-40" src="@/assets/lebron.jpg/" />
       <div>
-        <div class="text-center font-bold">Zesty Lebron</div>
-        <div>Vice President, Content Marketing and Chief Baller, ZEST FEST</div>
+        <div class="text-left font-bold">LeBonBon</div>
+        <div>Drop that shit like it's hot daddy 😍</div>
       </div>
+            <img class="rounded-full w-40 h-40" src="@/assets/dreamybull.jpg/" />
+      <div>
+        <div class="text-left font-bold">Ambatukam "DreamyBull" Ambassing</div>
+        <div>Ambatabusss, Ambatukam, Ambassinggg 😩</div>
+      </div>
+                  <img class="rounded-full w-40 h-40" src="@/assets/lilnas.jpg/" />
+      <div>
+        <div class="text-left font-bold">Little Nas X</div>
+        <div>Yeah this nigga gay asf.</div>
+      </div>
+                        <img class="rounded-full w-40 h-40" src="@/assets/dog.jpg/" />
+      <div>
+        <div class="text-left font-bold">This dog is super fucking gay 😭</div>
+        <div>Seriously who the fuck trained this nigga.</div>
+      </div>
+                              <img class="rounded-full w-40 h-40" src="@/assets/drake2.jpg/" />
+      <div>
+        <div class="text-left font-bold">BBL Drizzy again</div>
+        <div>Someone stop this nigga 😭😭</div>
+      </div>
+
     </div>
+    <br>
   </div>
+  
   <footer>
-    <p>© Copyright Zesty LeBron 2024</p>
+    <p>© Copyright Zesty Ass Niggas 2024</p>
   </footer>
 </template>
 
