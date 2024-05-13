c9629e4cb82a0471973007c9b1ba50fabf7ad9df
Eve_acad <coc123steve@gmail.com>
2024-05-14 00:35:35 +1000

    first commit

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -21,11 +21,11 @@
   </div>
   <div class="relative xl:hidden" v-if="show">
     <div class="absolute w-full bg-white flex flex-col items-center gap-3 py-3">
-      <a href="https://dexscreener.com/"><div class="flex gap-1.5">ZestyChart</div></a>
+      <a href="https://dexscreener.com/solana/8m8YSa6q3SB7z8yp3ecL13XvkqxFD24p3pb95DmgPo1f"><div class="flex gap-1.5">ZestyChart</div></a>
       <a href="https://t.me/zestfestsol"><div class="flex gap-1.5">ZestyGram</div></a>
-      <a href="https://twitter.com/"><div class="flex gap-1.5">ZesTwitter</div></a>
-      <a href="https://solscan.io/token/"><div class="flex gap-1.5">ZestyScan</div></a>
-      <a href="https://www.pump.fun/board">
+      <a href="https://twitter.com/zestfestsolana"><div class="flex gap-1.5">ZesTwitter</div></a>
+      <a href="https://solscan.io/token/8m8YSa6q3SB7z8yp3ecL13XvkqxFD24p3pb95DmgPo1f"><div class="flex gap-1.5">ZestyScan</div></a>
+      <a href="https://www.pump.fun/8m8YSa6q3SB7z8yp3ecL13XvkqxFD24p3pb95DmgPo1f">
       <button
         class="zest mx-5 px-4 text-xs gradient-blue text-white rounded-[4px] py-3 hover:border-[#3040E8] border hover:text-[#1829d9]" style="font-size: 30px"
       >
